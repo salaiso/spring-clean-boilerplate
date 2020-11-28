fb97ad9027a3542a5579f0e6a52ebf960281c662
Sami Alaiso <sami.alaiso@gmail.com>
2020-11-28 21:08:02 +0200

    - log example and banner off
    Signed-off-by: Sami Alaiso <sami.alaiso@gmail.com>

--- src/main/java/entrypoint/ItemEntryPoint.java
+++ src/main/java/entrypoint/ItemEntryPoint.java
@@ -1,6 +1,8 @@
 package entrypoint;
 
 import core.model.SessionData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import usecases.AddItemUseCase;
 import usecases.GetItemsUseCase;
 import core.model.Item;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 public class ItemEntryPoint {
 
+  Logger log = LoggerFactory.getLogger(ItemEntryPoint.class);
   protected GetItemsUseCase getItemsUseCase;
   protected AddItemUseCase addItemUseCase;
 
@@ -25,11 +28,13 @@ public class ItemEntryPoint {
 
   @GetMapping(path = "/items", produces = "application/json")
   public List<Item> getItems() {
+    log.info("GET /items");
     return getItemsUseCase.getItems();
   }
 
   @PutMapping(path = "/item", consumes = "application/json", produces = "application/json")
   public String putItem(@RequestBody Item item) {
+    log.info("PUT /item");
     return addItemUseCase.addItem(item, sessionData);
   }
 }
